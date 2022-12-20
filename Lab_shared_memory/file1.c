12a0918f2cffa02b824a0938d49c598027d75ccf
Vladislav Bunduki <69432124+VladPendalf@users.noreply.github.com>
2022-12-20 11:40:18 +0300

    Update file1.c

--- Lab_shared_memory/file1.c
+++ Lab_shared_memory/file1.c
@@ -5,7 +5,7 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <time.h>
-#include <string.c>
+#include <string.h>
 #include <stdlib.h>
 //==================TIME=============================
 time_t      rawtime;
